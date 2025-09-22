d917c70ada1b6fb3ff2a79306683703b9a7dc7de
fiqryomaratala <gaktaukepoaelu123@gmail.com>
2025-09-22 17:28:53 +0700

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/fiqryomaratala/simple-forum/internal/handlers/memberships"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a Gin router with default middleware (logger and recovery)
+	r := gin.Default()
+
+	membershipsHandler := memberships.NewHandler(r)
+	membershipsHandler.RegisterRoute()
+	r.Run(":9999")
+}
